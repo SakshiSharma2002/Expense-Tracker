159224e2a28882a14c4d08e7dd24cbaa57142676
yashikabhargava <76060661+yashikabhargava@users.noreply.github.com>
2022-04-22 20:22:27 +0530

    add expense tracker

---
+++ src/components/Main/Form/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  radioGroup: {
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'black',
+    marginBottom: '-10px',
+  },
+  button: {
+    marginTop: '20px',
+  },
+}));
